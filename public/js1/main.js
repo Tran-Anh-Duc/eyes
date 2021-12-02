3811cd49d0fd40aa3226bf6295a6d4bc7427dfb1
Tran-Anh-Duc <trananhducty@gmail.com>
2021-12-02 13:54:10 +0700

    them repo

--- public/js1/main.js
+++ public/js1/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
+    let data = [];
     $(".navbar-brand").html("jQuery");
-    $(".navbar-brand").click(welcome);
+    // $(".navbar-brand").click(welcome);
 
     function welcome() {
         alert("welcome")
@@ -15,21 +16,40 @@ $(document).ready(function () {
         }
     ).done(function (response) {
         console.log(response)
-        let html = "";
-        for (let i = 0; i < response.length; i++) {
-            // document.write(response[i].name+" "+response[i].id+"<br>");
-            html += `<tr>
-                      <th scope="row">${response[i].id}</th>
-                      <td>${response[i].name}</td>
-                    </tr>`
-        }
-        $(".list-git").html(html);
+        data= response;
+        displayGit(response)
+
     })
 
     function showError(error) {
         if (error.status === 404) {
             alert("sai duong dan")
         }
+    }
+
+    $("#search-input").on("input",search);
+
+    function search(){
+        let searchValue = $(this).val();
+        let result = [];
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].name.includes(searchValue)){
+                result.push(data[i]);
+            }
+        }
+        displayGit(result);
+    }
+
+    function displayGit(gits){
+        let html = "";
+        for (let i = 0; i < gits.length; i++) {
+            // document.write(response[i].name+" "+response[i].id+"<br>");
+            html += `<tr>
+                      <th scope="row">${gits[i].id}</th>
+                      <td>${gits[i].name}</td>
+                    </tr>`
+        }
+        $(".list-git").html(html);
     }
 
 });
