d641cb6280140f7e03b2cebd2ee183c042971d75
Tran-Anh-Duc <trananhducty@gmail.com>
2021-12-02 13:12:13 +0700

    them phan repo

--- public/js1/main.js
+++ public/js1/main.js
@@ -1,20 +1,35 @@
-$(document).ready(function (){
+$(document).ready(function () {
     $(".navbar-brand").html("jQuery");
     $(".navbar-brand").click(welcome);
 
-    function welcome(){
+    function welcome() {
         alert("welcome")
     }
 
 
-
     $.ajax(
         {
-            url:"https://api.github.com/users/Tran-Anh-Duc/repos",
-            type:"GET",
-            dataType:"json",
+            url: "https://api.github.com/users/Tran-Anh-Duc/repos",
+            type: "GET",
+            dataType: "json",
         }
-    ).done(function (response){
+    ).done(function (response) {
         console.log(response)
+        let html = "";
+        for (let i = 0; i < response.length; i++) {
+            // document.write(response[i].name+" "+response[i].id+"<br>");
+            html += `<tr>
+                      <th scope="row">${response[i].id}</th>
+                      <td>${response[i].name}</td>
+                    </tr>`
+        }
+        $(".list-git").html(html);
     })
+
+    function showError(error) {
+        if (error.status === 404) {
+            alert("sai duong dan")
+        }
+    }
+
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,6 +46,30 @@
     </div>
 </nav>
 
+<table class="table table-dark " style="width: 500px;margin-left: 200px">
+    <thead>
+    <tr>
+        <th scope="col">id</th>
+        <th scope="col">name</th>
+    </tr>
+    </thead>
+    <tbody class="list-git">
+
+    <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+    </tr>
+    </tbody>
+</table>
+
 <script src="{{asset('js1/main.js')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
